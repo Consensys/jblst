75031380224babae847baeab91b8375c70784d0a
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2020-09-15 21:24:02 +0300

    Support x86_64 'os.arch' value for MacOS

--- src/main/java/tech/pegasys/teku/bls/impl/blst/JBlst.java
+++ src/main/java/tech/pegasys/teku/bls/impl/blst/JBlst.java
@@ -1,14 +1,32 @@
 package tech.pegasys.teku.bls.impl.blst;
 
+import java.util.Map;
+
 public class JBlst {
 
+  private static final Map<String, String> ARCH_MAPPING = Map.of(
+      "x86_64", "x86_64",
+      "amd64", "x86_64",
+      "aarch64", "aarch64"
+  );
+
   public static void loadNativeLibrary() throws UnsupportedOperationException {
-    String libLocation = "/" + System.getProperty("os.arch") + "/" + System.mapLibraryName("jblst");
+    String libLocation = "/" + getArch() + "/" + System.mapLibraryName("jblst");
     try {
       NativeUtils.loadLibraryFromJar(libLocation);
     } catch (Throwable e) {
       throw new UnsupportedOperationException(
           "Couldn't load native BLS library (from resource " + libLocation + "): ", e);
     }
+  }
+
+  private static String getArch() {
+    String osArch = System.getProperty("os.arch");
+    String arch = ARCH_MAPPING.get(osArch);
+    if (arch == null) {
+      throw new UnsupportedOperationException(
+          "Unknown arch value from 'os.arch' property: '" + osArch + "'");
+    }
+    return arch;
   }
 }
