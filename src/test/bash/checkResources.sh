1d76cd01ada4c456c365e7806f75602719f2e579
Adrian Sutton <adrian.sutton@consensys.net>
2022-04-08 11:43:31 +1000

    Check assembled jar contains all the expected platform files

---
+++ src/test/bash/checkResources.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -euo pipefail
+
+JAR=${1:?Must specify path to built jblst jar file}
+
+CONTENTS=`jar tvf ${JAR}`
+
+EXPECTED="Linux/aarch64/libblst.so
+Linux/amd64/libblst.so
+Linux/optimised/aarch64/libblst.so
+Linux/optimised/amd64/libblst.so
+Mac/aarch64/libblst.dylib
+Mac/x86_64/libblst.dylib
+Mac/optimised/aarch64/libblst.dylib
+Mac/optimised/x86_64/libblst.dylib
+Windows/amd64/blst.dll
+Windows/optimised/amd64/blst.dll"
+
+EXIT_CODE=0
+
+
+for LIB in $EXPECTED
+do
+  echo -n "Checking for $LIB: "
+  if [[ "$CONTENTS" == *"$LIB"* ]]
+  then
+    echo "Present"
+  else
+    echo "Missing!"
+    EXIT_CODE=1
+  fi
+done
+
+exit $EXIT_CODE
